4fb8aa951df4a3d4882bea5f12d738454110dc14
Neil Tan <neil.c.tan@gmail.com>
2018-03-31 22:38:01 +0800

    removed the fancy hook

--- deep_mlp.py
+++ deep_mlp.py
@@ -183,7 +183,12 @@ def main(_):
     testing_init_op = iterator.make_initializer(ds_test)
     
   (x, y_) = iterator.get_next()
-  
+
+  tf.summary.image(
+    "input images",
+    tf.reshape(x, [-1, 28, 28, 1]),
+    max_outputs=8)
+
   # Build the graph for the deep net
   y_pred = deepnn(x)
 
@@ -199,41 +204,20 @@ def main(_):
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name="accuracy")
     tf.summary.scalar('accuracy', accuracy)
 
-  class _LoggerHook(tf.train.SessionRunHook):
-    """Logs loss and runtime."""
-    def begin(self):
-      self._step = -1
-      self._start_time = time.time()
-    def before_run(self, run_context):
-      self._step += 1
-      return tf.train.SessionRunArgs(loss)  # Asks for loss value.
-    def after_run(self, run_context, run_values):
-      if self._step % FLAGS.log_frequency == 0:
-        current_time = time.time()
-        duration = current_time - self._start_time
-        self._start_time = current_time
-        loss_value = run_values.results
-        examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
-        sec_per_batch = float(duration / FLAGS.log_frequency)
-        format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                      'sec/batch)')
-        print (format_str % (datetime.now(), self._step, loss_value,
-                            examples_per_sec, sec_per_batch))
   with tf.train.MonitoredTrainingSession(
       checkpoint_dir=FLAGS.chk_dir,
       hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
              tf.train.NanTensorHook(loss),
-             _LoggerHook()],
+             tf.train.SessionRunHook()],
       config=tf.ConfigProto(
           log_device_placement=FLAGS.log_device_placement)) as mon_sess:
 
     while not mon_sess.should_stop():
       mon_sess.run(training_init_op)
       mon_sess.run(train_step)
-      if global_step.eval(mon_sess) % 100 == 0:
+      if global_step.eval(mon_sess) % 20 == 0:
         mon_sess.run(testing_init_op)
-        tf.summary.scalar('test accuracy', accuracy)
-        print('test accuracy %g' % accuracy.eval(mon_sess))
+        print('test accuracy %g' % accuracy.eval(session=mon_sess))
 
 
 if __name__ == '__main__':
